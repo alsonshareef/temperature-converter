1d6ebdc087f378c196a1dd69488fca775dfdfd42
Alson Shareef <alson.shareef@gmail.com>
2018-12-07 16:50:33 +1100

    .

--- index.js
+++ index.js
@@ -14,14 +14,12 @@ const F_to_C = (F) => {
 let chooseConversion = ['Celcius to Fahrenheit', 'Fahrenheit to Celcius'];
 let index = readlineSync.keyInSelect(chooseConversion, 'Hi there, what type of conversion are you looking to do?');
 
-if (index === 0) {
-    let input = readlineSync.question('Please enter the degrees in Celcius: ')
-    console.log(C_to_F(input))
-} else if (index === 1) {
-    let input = readlineSync.question('Please enter the degrees in Fahrenheit: ')
-    console.log(F_to_C(input))
-} else {
-    console.log('bye')
-}
-
-// Redirect to Menu
+    if (index === 0) {
+        let input = readlineSync.question('Please enter the degrees in Celcius: ')
+        console.log(C_to_F(input))
+    } else if (index === 1) {
+        let input = readlineSync.question('Please enter the degrees in Fahrenheit: ')
+        console.log(F_to_C(input))
+    } else {
+        console.log('bye')
+    }
